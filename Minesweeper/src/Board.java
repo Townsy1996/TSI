531870d987706435d4f071aa3b9a4d49341a1592
corey <corey_townsend@outlook.com>
2024-01-29 15:49:16 +0000

    First commit

---
+++ Minesweeper/src/Board.java
@@ -0,0 +1,15 @@
+public class Board {
+
+    private final int[][] gameBoard = new int [10][10];
+    private int numMines = 15;
+
+    public Board(){
+    }
+
+    public void printBoard(){
+
+
+
+    }
+}
+
